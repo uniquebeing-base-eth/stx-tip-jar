284e48d710a0df92000ec2e5d8c0b6ede28a3b8b
Uniquebeing <beingunique049@gmail.com>
2026-02-11 15:51:49 +0100

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,4 +1,3 @@
-
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
@@ -55,3 +54,5 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   },
 );
 Button.displayName = "Button";
+
+export { Button, buttonVariants };
