c4381f3e86c5cc2c3935e3f1c3aa63c35e4fcba4
Uniquebeing <beingunique049@gmail.com>
2026-02-16 00:40:58 +0100

    Update pagination.tsx

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -5,6 +5,7 @@ import { ChevronLeft, ChevronRight, MoreHorizontal } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { ButtonProps, buttonVariants } from "@/components/ui/button";
 
+
 // Root pagination container
 const Pagination = ({ className, ...props }: React.ComponentProps<"nav">) => (
   <nav
