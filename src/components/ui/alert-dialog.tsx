97b67ab24b73f06e3229cff23c41773143fc44fc
Uniquebeing <beingunique049@gmail.com>
2026-02-16 00:48:25 +0100

    Update alert-dialog.tsx

--- src/components/ui/alert-dialog.tsx
+++ src/components/ui/alert-dialog.tsx
@@ -5,6 +5,7 @@ import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog";
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
 
+
 const AlertDialog = AlertDialogPrimitive.Root;
 
 const AlertDialogTrigger = AlertDialogPrimitive.Trigger;
