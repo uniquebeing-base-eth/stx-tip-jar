82a499a47dbed4ec33de05c0e078a37fc63e8d0c
Uniquebeing <beingunique049@gmail.com>
2026-02-16 00:37:29 +0100

    Update slider.tsx

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -5,6 +5,7 @@ import * as SliderPrimitive from "@radix-ui/react-slider";
 
 import { cn } from "@/lib/utils";
 
+
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
