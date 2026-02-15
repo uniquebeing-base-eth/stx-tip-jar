170d494a72da27ed4478054536b91659df1c1796
Uniquebeing <beingunique049@gmail.com>
2026-02-16 00:42:42 +0100

    Update hover-card.tsx

--- src/components/ui/hover-card.tsx
+++ src/components/ui/hover-card.tsx
@@ -4,6 +4,7 @@ import * as HoverCardPrimitive from "@radix-ui/react-hover-card";
 
 import { cn } from "@/lib/utils";
 
+
 const HoverCard = HoverCardPrimitive.Root;
 
 const HoverCardTrigger = HoverCardPrimitive.Trigger;
