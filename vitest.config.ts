857fd7be4a4f8b1ed44f6edebca35b5c32ae841e
Uniquebeing <beingunique049@gmail.com>
2026-02-16 01:05:26 +0100

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
+
 export default defineConfig({
   plugins: [react()],
   test: {
