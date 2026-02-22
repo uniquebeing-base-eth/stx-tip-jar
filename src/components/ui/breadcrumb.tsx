e04de6edc6a6cd8827f38242f0400db4443e9107
Uniquebeing <beingunique049@gmail.com>
2026-02-22 11:40:52 +0100

    Update breadcrumb.tsx

--- src/components/ui/breadcrumb.tsx
+++ src/components/ui/breadcrumb.tsx
@@ -5,6 +5,7 @@ import { ChevronRight, MoreHorizontal } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 
+
 const Breadcrumb = React.forwardRef<
   HTMLElement,
   React.ComponentPropsWithoutRef<"nav"> & {
