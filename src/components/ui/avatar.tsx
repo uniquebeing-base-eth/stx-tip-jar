3e1e7961607d62f912faddf324280acaaa941a2f
Uniquebeing <beingunique049@gmail.com>
2026-02-22 11:40:11 +0100

    Update avatar.tsx

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -4,6 +4,7 @@ import * as AvatarPrimitive from "@radix-ui/react-avatar";
 
 import { cn } from "@/lib/utils";
 
+
 const Avatar = React.forwardRef<
   React.ElementRef<typeof AvatarPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
